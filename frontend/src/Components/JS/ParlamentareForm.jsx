db093c69d10b2596438e5aa5177c0c794cd9d287
LorenaaTr <lorenatroshupa@gmail.com>
2023-06-07 18:59:56 +0200

    voting form css

--- frontend/src/Components/JS/ParlamentareForm.jsx
+++ frontend/src/Components/JS/ParlamentareForm.jsx
@@ -3,41 +3,69 @@ import '../CSS/parlamentare.css';
 import { Link } from 'react-router-dom';
 
 class ParlamentareForm extends Component {
-    render() {
+  generateCheckboxes(count) {
+    const checkboxes = [];
 
-            return (
-                <>
-                    <div className="divClass">
-                        <h1 className="header1">Zgjedhjet Parlamentare jane HAPUR!</h1>
-                        <p className="paragraphs">Ne menyre qe vota te jete e vlefshe, zgjedhni partin dhe numrat perkates te 5 kandidateve te asaj partie.</p>
-
-                        <div className='flexbox-container'>
-                            <div className='flexbox-item flexbox-item-1'>
-                                <label>
-                                     1
-                                    <input
-                                    type="checkbox"
-                                    />
-                                    
-                                    </label>
-                            </div>
-                            <div className='flexbox-item flexbox-item-2'></div>
-                            <div className='flexbox-item flexbox-item-3'></div>
-                            <div className='flexbox-item flexbox-item-4'></div>
-                            <div className='flexbox-item flexbox-item-5'></div>
-                        </div>
-                     
-                          
-
-                        <Link to="">
-                            <button className="button1">Dergo Voten!</button>
-                        </Link>
-                    </div>
-                </>
-            );
-        }
+    for (let i = 1; i <= count; i++) {
+      checkboxes.push(
+        <label key={i}>
+          <input type="checkbox" />
+          {i}
+        </label>
+      );
     }
 
+    return checkboxes;
+  }
+
+  render() {
+    return (
+      <>
+        <div className="divClass">
+          <h1 className="header1">Zgjedhjet Parlamentare jane HAPUR!</h1>
+          <p className="paragraphs">
+            Ne menyre qe vota te jete e vlefshme, zgjedhni partin dhe numrat perkates te 5 kandidateve te asaj partie.
+          </p>
+
+          <div className="flexbox-container">
+          <label>
+              <input type="checkbox" />
+              133 LVV
+            </label>
+
+            <label>
+              <input type="checkbox" />
+              134 PDK
+            </label>
+
+            <label>
+              <input type="checkbox" />
+              135 LDK
+            </label>
+
+            <label>
+              <input type="checkbox" />
+              136 AAK
+            </label>
+
+            <label>
+              <input type="checkbox" />
+              137 AKR
+            </label>
+           
+          </div>
+
+          <div className='checkbox2'>
+            {this.generateCheckboxes(120)}
+            </div>
+
+          <Link to="">
+            <button className="button1">Dergo Voten!</button>
+          </Link>
+        </div>
+      </>
+    );
+  }
+}
 
 export default ParlamentareForm;
-
