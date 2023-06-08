91fed3edb67c6235703bdb891d8f410f15b042b5
LorenaaTr <lorenatroshupa@gmail.com>
2023-06-08 19:37:09 +0200

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,11 +13,7 @@ import CreateUser from './userCRUD/createUser.jsx';
 import Read from './userCRUD/readUser.jsx';
 import Edit from './userCRUD/editUser.jsx';
 import ParlamentareForm from './Components/JS/ParlamentareForm.jsx';
-<<<<<<< HEAD
 
-=======
-import votaLokale from './Components/JS/votaLokale.jsx';
->>>>>>> parent of 5b58f915 (.)
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -38,12 +34,7 @@ function App() {
                     <Route path='/read/:id' element={<Read />} />
                     <Route path='/edit/:id' element={<Edit />} />
                     <Route path="/ParlamentareForm" element={<ParlamentareForm />} />
-<<<<<<< HEAD
-                    
-=======
-                    <Route path="/votaLokale" element={<votaLokale />} />
->>>>>>> parent of 5b58f915 (.)
-                </Routes>
+                    </Routes>
             </Router>
         </>
     )
