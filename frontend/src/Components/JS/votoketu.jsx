e46b1d58c5858d3db36c4f3544fab0237539cea4
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-06-08 23:25:08 +0300

    .

---
+++ frontend/src/Components/JS/votoketu.jsx
@@ -0,0 +1,26 @@
+import Header from '../Header.jsx';
+import Footer from '../Footer.jsx';
+import Sidebar from '../Sidebar';
+import React from "react";
+import '../CSS/voto.css';
+
+function Voto(){
+    return(
+    <div>
+        <Navbar/>
+        <Sidebar/>
+        <div className="zgjedhjet">
+            <h1>Voto!</h1>
+            <div className="zgjedhjetQendrore">
+                <h3>Zgjedhjet Qendrore</h3>
+            </div>
+            <div className="zgjedhjetLokale">
+                <h3>Zgjedhjet Lokale</h3>
+            </div>
+        </div>
+        <Footer/>
+    </div>
+    )
+}
+
+export default Voto;
